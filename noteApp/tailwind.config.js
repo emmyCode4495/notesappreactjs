481f0846f3649d88fba59152ac6e35c1c959e588
Emmanuel Pius <emmycode4495@gmail.com>
2024-08-18 07:03:03 +0100

    Installed tailwindcss and created the login and signup pages

---
+++ noteApp/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+    },
+    colors:{
+      primary:'#2B85FF',
+      secondary:'#EF863E',
+      white:'#fdfeff',
+      transparent: 'transparent',
+      blue:'#2B85FF',
+      red: '#FF0000'
+    }
+  },
+  plugins: [],
+}
+
